97317a17b71c250495f913af5a6e85a81439adff
jonaskaz <jonas.kazlauskas@me.com>
2020-12-11 23:09:02 -0500

    Completes proof of concept

--- train_model.m
+++ train_model.m
@@ -11,7 +11,7 @@ function [x_model, y_model] = train_model(filename_vec)
         accel_x = accel_x(1:120,:);
         accel_y = accel_y(1:120,:);
         accel_z = accel_z(1:120,:);
-        char_x = find_fft(accel_x);
+        [freq char_x] = find_fft(accel_x);
         x_model(:,file) =  char_x;
 
         char_y = find_fft(accel_y);
