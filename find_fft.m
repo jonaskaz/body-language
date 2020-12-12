3c12e770055e9e774944b457abffae64a470d384
Marion Madanguit <mmadanguit@olin.edu>
2020-12-12 14:45:07 -0500

    Undetectable changes to find_fft.m

--- find_fft.m
+++ find_fft.m
@@ -5,7 +5,7 @@
 function [frequencies_shift, y_shift] = find_fft(accel)
     N = length(accel);
     frequencies_shift = linspace(-pi, pi-2/N*pi, N) + pi/N*mod(N,2);
-    y_shift = fftshift(real(fft(accel)));
+    y_shift = fftshift(abs(fft(accel)));
     %fft_vec = fft(accel);
     %stem(frequencies_shifted, abs(fftshift(fft_vec)))
 end
